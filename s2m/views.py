2057dc0700fd1d9eaf6a594d73cafa199bc06d0f
Streichholzschaechtelchen <francois.hublet@polytechnique.edu>
2018-03-17 19:14:50 +0100

    Suite bugfix

--- s2m/views.py
+++ s2m/views.py
@@ -140,15 +140,10 @@ def validate_choice(request):
     
 @login_required
 def help_construction(request):
-<<<<<<< HEAD
     print(request.POST['query'])
-    response = json.dumps(s2m_parser.help(request.POST['query']))
-    print(response)
-    return HttpResponse(response)
-=======
     if request.POST['query'] != "" :
         response = json.dumps(s2m_parser.help(request.POST['query']))
     else :
         response = json.dumps([])
+    print(response)
     return HttpResponse(response)
->>>>>>> ff28c4a8da2af63e56822673be937a1ab910da40
